d3fbb9a4cd41c553f1c75a8a93143fa2d77a9877
JustHard <1411761044@qq.com>
2017-10-22 21:19:05 +0800

    实验二单词本

--- app/src/androidTest/java/com/example/words/ApplicationTest.java
+++ app/src/androidTest/java/com/example/words/ApplicationTest.java
@@ -2,10 +2,7 @@ package com.example.words;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
-
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
+//application
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
